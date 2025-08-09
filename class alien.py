c576b1e5240b79715781a03e91b9cb20b6ff42af
QBrer <3093815036@qq.com>
2025-08-09 09:59:12 +0800

    Changes

--- class alien.py
+++ class alien.py
@@ -32,22 +32,46 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            # elif event.type == pygame.KEYDOWN:
+            #     if event.key == pygame.K_RIGHT:
+            #         #向右移动
+            #         # self.ship.rect.x += 1
+            #         self.ship.moving_right = True
+            #     elif event.key == pygame.K_LEFT:
+            #         #向左移动
+            #         # self.ship.rect.x -= 1
+            #         self.ship.moving_left = True
+            # elif event.type == pygame.KEYUP:
+            #     if event.key == pygame.K_RIGHT:
+            #         #停止向右移动
+            #         # self.ship.rect.x -= 1
+            #         self.ship.moving_right = False
+            #     elif event.key == pygame.K_LEFT:
+            #         self.ship.moving_left = False
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    #向右移动
-                    # self.ship.rect.x += 1
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    #向左移动
-                    # self.ship.rect.x -= 1
-                    self.ship.moving_left = True
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    #停止向右移动
-                    # self.ship.rect.x -= 1
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """响应按键按下事件"""
+        if event.key == pygame.K_RIGHT:
+            #向右移动
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            #向左移动
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        """响应按键松开事件"""
+        if event.key == pygame.K_RIGHT:
+            #停止向右移动
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
     def _update_screen(self):
         # 每次循环都重绘屏幕
         self.screen.fill(self.settings.bg_color)
